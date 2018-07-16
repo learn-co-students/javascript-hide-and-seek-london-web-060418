37261a34ef1d88cf7e16aa5ba43c3b115c5a06de
github username <github email address>
2018-07-16 17:46:28 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString();
+  }
+}
+
+
+function deepestChild(){
+  const grand = document.getElementById('grand-node');
+  var div = grand.children[0];
+
+  for (var i = 0; i < 3; i++) {
+    div = div.children[0];
+  }
+  console.log(div);
+  return div;
+}
